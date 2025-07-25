b878c3bdf83fd0da896d2b77a0ea99bfba90ba92
Trần Đức Trình <tranductrinh2k4@gmail.com>
2025-07-25 17:55:00 +0700

    feat(config): Add environment variable validation with Zod
    Introduced a shared environment variable validator using Zod and
    integrated it with the global NestJS ConfigModule.
    
    This ensures all required environment variables are present and valid
    at application startup, improving configuration safety and reliability.

---
+++ shared/config/environment-var-validator.config.ts
@@ -0,0 +1,17 @@
+
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string().url(),
+  ACCESS_TOKEN_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRES_IN: z.string(),
+  REFRESH_TOKEN_SECRET: z.string(),
+  REFRESH_TOKEN_EXPIRES_IN: z.string(),
+  SECRET_API_KEY: z.string(),
+});
+export function validateEnv(config: Record<string, unknown>) {
+  const validatedConfig = envSchema.parse(config);
+
+  return validatedConfig;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from 'shared/config/environment-var-validator.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 3. Giúp ConfigModule có sẵn ở mọi nơi
+      envFilePath: '.env', // 4. Chỉ định đường dẫn file .env
+      validate: validateEnv, // 5. Giao "bản quy tắc" để kiểm tra
+      cache: true, // 6. Tăng tốc độ truy cập biến môi trường
+    }),
+  ],
   controllers: [],
   providers: [],
 })
